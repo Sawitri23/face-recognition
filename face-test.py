b53c03ee38552df9bacd5d3edb9a6890c10fd528
Sawitri Sairaksa <sawitai.s@ku.th>
2023-10-06 04:06:55 +0700

    up

---
+++ face-test.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageDraw
+import face_recognition
+import numpy as np
+import pickle
+
+known_face_names, known_face_encodings = pickle.load(open('faces.p', 'rb'))
+
+image = Image.open('test/group1.jpg')
+
+face_locations = face_recognition.face_locations(np.array(image))
+face_encodings = face_recognition.face_encodings(np.array(image), face_locations)
+
+draw = ImageDraw.Draw(image)
+
+for face_encoding, face_location in zip(face_encodings, face_locations):
+    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+    best_match_index = np.argmin(face_distances)
+    print(face_distances)
